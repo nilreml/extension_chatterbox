e3e3581df7f61664e07a601f44b8dc5915840d22
Merlin Lüdicke <merlin.luedicke@gmail.com>
2025-06-30 11:48:13 +0200

    feat: switch chatterbox to fork with min_p sampling

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-
 import setuptools
-import re
-import os
 
 setuptools.setup(
-	name="extension_chatterbox",
+    name="extension_chatterbox",
     packages=setuptools.find_namespace_packages(),
     version="2.0.1",
-	author="rsxdalv",
-	description="Chatterbox TTS extension for text-to-speech generation.",
-	url="https://github.com/rsxdalv/extension_chatterbox",
+    author="rsxdalv",
+    description="Chatterbox TTS extension for text-to-speech generation.",
+    url="https://github.com/rsxdalv/extension_chatterbox",
     project_urls={},
     scripts=[],
     install_requires=[
         "gradio",
         # "chatterbox-tts"
-        "chatterbox-tts @ git+https://github.com/rsxdalv/chatterbox@fast",
+        # "chatterbox-tts @ git+https://github.com/rsxdalv/chatterbox@fast",
+        "chatterbox-tts @ git+https://github.com/nilreml/chatterbox@fast",
         # "peft",
     ],
     classifiers=[
